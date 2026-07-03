5850bf3a69dced07bc95779783e7788ea1caf9b0
KevenGomes <kevengustavo2012@gmail.com>
2026-07-03 11:55:17 -0300

    feat(silver): add procedure to load silver data, and adjust silver DDL

--- scripts/silver/03_ddl_silver.sql
+++ scripts/silver/03_ddl_silver.sql
@@ -22,20 +22,21 @@ CREATE TABLE silver.crm_cust_info(
     cst_marital_status nvarchar(50),
     cst_gndr nvarchar(50),
     cst_create_date date,
-    dwh_create_date date default getdate(),
+    dwh_create_date datetime2 default getdate(),
 );
 
 if object_id('silver.crm_prd_info') is not null
     drop table silver.crm_prd_info;
 create table silver.crm_prd_info(
     prd_id int,
+    cat_id nvarchar(50),
     prd_key nvarchar(50),
     prd_nm nvarchar(50),
     prd_cost int,
     prd_line nvarchar(50),
     prd_start_dt date,
     prd_end_dt date,
-    dwh_create_date date default getdate(),
+    dwh_create_date datetime2 default getdate(),
 );
 
 if object_id('silver.crm_sales_details') is not null
@@ -44,13 +45,13 @@ create table silver.crm_sales_details(
     sls_ord_num NVARCHAR(50),
     sls_prd_key NVARCHAR(50),
     sls_cust_id int,
-    sls_order_dt int,
-    sls_ship_dt int,
-    sls_due_dt int,
+    sls_order_dt date,
+    sls_ship_dt date,
+    sls_due_dt date,
     sls_sales int,
     sls_quantity int,
     sls_price int,
-    dwh_create_date date default getdate(),
+    dwh_create_date datetime2 default getdate(),
 );
 
 --create silver erp tables
@@ -61,7 +62,7 @@ create table silver.erp_cust_az12(
     cid nvarchar(50),
     bdate date,
     gen nvarchar(50),
-    dwh_create_date date default getdate(),
+    dwh_create_date datetime2 default getdate(),
 );
 
 if object_id('silver.erp_loc_a101') is not null
@@ -69,7 +70,7 @@ if object_id('silver.erp_loc_a101') is not null
 create table silver.erp_loc_a101(
     cid nvarchar(50),
     cntry nvarchar(50),
-    dwh_create_date date default getdate(),
+    dwh_create_date datetime2 default getdate(),
 );
 
 if object_id('silver.erp_px_cat_g1v2') is not null
@@ -79,5 +80,5 @@ create table silver.erp_px_cat_g1v2(
     cat nvarchar(50),
     subcat nvarchar(50),
     maintenance nvarchar(50),
-    dwh_create_date date default getdate()
+    dwh_create_date datetime2 default getdate()
 );  
---
+++ scripts/silver/04_proc_load_silver.sql
@@ -0,0 +1,225 @@
+/*
+=============================================================
+Create procedure to Load Data into Silve Layer
+=============================================================
+Script Purpose:
+    This script is designed to load data into the silver layer of the DataWarehouse database.
+*/
+
+create or alter procedure silver.load_silver_data as
+begin    
+    declare @start_time datetime, @proc_start_time datetime = SYSDATETIME();
+    
+    begin try
+        print '=============================================================';
+        print 'Loading data into silver layer';
+        print '=============================================================';
+
+        print '-------------------------------------------------------------';
+        print 'Loading CRM Tables';
+        print '-------------------------------------------------------------';
+
+        set @start_time = SYSDATETIME();
+        print '>> Truncating Table: silver.crm_cust_info';
+        truncate table silver.crm_cust_info;
+        print '>> Loading Table: silver.crm_cust_info';
+        insert into silver.crm_cust_info(
+            cst_id,
+            cst_key,
+            cst_first_name,
+            cst_last_name,
+            cst_marital_status,
+            cst_gndr,
+            cst_create_date
+        )
+        select 
+            cst_id,
+            cst_key,
+            trim(cst_first_name) as cst_first_name,
+            trim(cst_last_name) as cst_last_name,
+            case
+                when upper(trim(cst_marital_status)) = 'M' then 'Married'
+                when upper(trim(cst_marital_status)) = 'S' then 'Single'
+                else 'N/A'
+            end as cst_marital_status,
+            case
+                when upper(trim(cst_gndr)) = 'M' then 'Male'
+                when upper(trim(cst_gndr)) = 'F' then 'Female'
+                else 'N/A'
+            end as cst_gndr,
+            cst_create_date
+        from(
+            select 
+                *, 
+                row_number() over (partition by cst_id order by cst_create_date desc) as flag_older
+            from bronze.crm_cust_info
+            where cst_id is not null
+        ) as rknd_cust_info 
+        where flag_older = 1;
+
+        print '>> Load duration: ' + cast(DATEDIFF(MILLISECOND, @start_time, SYSDATETIME()) as varchar(20)) + ' ms';
+        print '-------------------------------------------------------------';
+
+        set @start_time = SYSDATETIME();
+        print '>> Truncating Table: silver.crm_prd_info';
+        truncate table silver.crm_prd_info;
+        print '>> Loading Table: silver.crm_prd_info';
+        insert into silver.crm_prd_info(
+            prd_id,
+            cat_id,
+            prd_key,
+            prd_nm,
+            prd_cost,
+            prd_line,
+            prd_start_dt,
+            prd_end_dt
+        )
+        select 
+            prd_id,
+            replace(substring(prd_key, 1, 5),'-','_' ) as cat_id,
+            substring(prd_key, 7, len(prd_key)) as prd_key,
+            prd_nm,
+            isnull(prd_cost, 0) as prd_cost,
+            case
+                when prd_line = 'M' then 'Mountain'
+                when prd_line = 'R' then 'Road'
+                when prd_line = 'T' then 'Touring'
+                when prd_line = 'S' then 'Other Sales'
+                else 'N/A'
+            end as prd_line,
+            prd_start_dt,
+            dateadd(day, -1, LEAD(prd_start_dt) over(partition by prd_key order by prd_start_dt asc)) as prd_end_dt
+        from bronze.crm_prd_info;
+
+        print '>> Load duration: ' + cast(DATEDIFF(MILLISECOND, @start_time, SYSDATETIME()) as varchar(20)) + ' ms';
+        print '-------------------------------------------------------------';
+
+        set @start_time = SYSDATETIME();
+        print '>> Truncating Table: silver.crm_sales_details';
+        truncate table silver.crm_sales_details;
+        print '>> Loading Table: silver.crm_sales_details';
+        insert into silver.crm_sales_details(
+            sls_ord_num,
+            sls_prd_key,
+            sls_cust_id,
+            sls_order_dt,
+            sls_ship_dt,
+            sls_due_dt,
+            sls_sales,
+            sls_quantity,
+            sls_price
+        )
+        select
+            sls_ord_num,
+            sls_prd_key,
+            sls_cust_id,
+            case
+                when sls_order_dt = 0 or len(sls_order_dt) != 8 then null
+                else cast(cast(sls_order_dt as varchar(8)) as date)
+            end as sls_order_dt,
+            case
+                when sls_ship_dt = 0 or len(sls_ship_dt) != 8 then null
+                else cast(cast(sls_ship_dt as varchar(8)) as date)
+            end as sls_ship_dt,
+            case
+                when sls_due_dt = 0 or len(sls_due_dt) != 8 then null
+                else cast(cast(sls_due_dt as varchar(8)) as date)
+            end as sls_due_dt,
+            case
+                when sls_sales is null or sls_sales <= 0 or sls_sales != sls_quantity * ABS(sls_price) 
+                    then sls_quantity * ABS(sls_price)
+                else sls_sales
+            end as sls_sales,
+            sls_quantity,
+            case
+                when sls_price is null or sls_price <= 0 
+                    then sls_sales / nullif(sls_quantity, 0)
+                else sls_price
+            end as sls_price
+        from bronze.crm_sales_details;
+
+        print '>> Load duration: ' + cast(DATEDIFF(MILLISECOND, @start_time, SYSDATETIME()) as varchar(20)) + ' ms';
+        print '-------------------------------------------------------------';
+        print 'Loading ERP Tables';
+        print '-------------------------------------------------------------';
+
+        set @start_time = SYSDATETIME();
+        print '>> Truncating Table: silver.erp_cust_az12';
+        truncate table silver.erp_cust_az12;
+        print '>> Loading Table: silver.erp_cust_az12';
+        insert into silver.erp_cust_az12(
+            cid,
+            bdate,
+            gen
+        )
+        select 
+            case when cid like 'NAS%' then substring(cid,4, len(cid))
+                else cid
+            end as cid,
+            case when bdate > getdate() then null
+                else bdate
+            end as bdate,
+            case when upper(trim(REPLACE(gen, CHAR(13), ''))) in ('F', 'FEMALE') then 'Female'
+                when upper(trim(REPLACE(gen, CHAR(13), ''))) in ('M', 'MALE') then 'Male'
+                else 'N/A'
+            end as gen
+        from bronze.erp_cust_az12;
+
+        print '>> Load duration: ' + cast(DATEDIFF(MILLISECOND, @start_time, SYSDATETIME()) as varchar(20)) + ' ms';
+        print '-------------------------------------------------------------';
+
+        set @start_time = SYSDATETIME();
+        print '>> Truncating Table: silver.erp_loc_a101';
+        truncate table silver.erp_loc_a101;
+        print '>> Loading Table: silver.erp_loc_a101';
+        insert into silver.erp_loc_a101(
+            cid,
+            cntry
+        )
+        select 
+            replace(cid,'-','') as cid,
+            case when upper(trim(replace(cntry,char(13),''))) in ('USA','US') then 'United States'
+                when upper(trim(replace(cntry,char(13),''))) = 'DE' then 'Germany'
+                when trim(replace(cntry,char(13),'')) in ('',null) then 'N/A'
+                else replace(cntry,char(13),'') 
+            end as cntry
+        from bronze.erp_loc_a101;
+        
+        print '>> Load duration: ' + cast(DATEDIFF(MILLISECOND, @start_time, SYSDATETIME()) as varchar(20)) + ' ms';
+        print '-------------------------------------------------------------';
+
+        set @start_time = SYSDATETIME();
+        print '>> Truncating Table: silver.erp_px_cat_g1v2';
+        truncate table silver.erp_px_cat_g1v2;
+        print '>> Loading Table: silver.erp_px_cat_g1v2';
+        insert into silver.erp_px_cat_g1v2(
+            id,
+            cat,
+            subcat,
+            maintenance
+        )
+        select 
+            id,
+            cat,
+            subcat,
+            replace(maintenance, char(13), '') as maintenance
+        from bronze.erp_px_cat_g1v2;
+
+        print '>> Load duration: ' + cast(DATEDIFF(MILLISECOND, @start_time, SYSDATETIME()) as varchar(20)) + ' ms';
+        print '-------------------------------------------------------------';
+
+        print '=============================================================';
+        print 'Data loading completed successfully';
+        print 'Duration: ' + cast(DATEDIFF(MILLISECOND, @proc_start_time, SYSDATETIME()) as varchar(20)) + ' ms';
+        print '=============================================================';
+    end try
+    begin catch
+        print '=============================================================';
+        print 'Error occurred while loading data into silver layer';
+        print 'Error Number: ' + cast(error_number() as varchar(10));
+        print 'Error Severity: ' + cast(error_severity() as varchar(10));
+        print 'Error State: ' + cast(error_state() as varchar(10));
+        print 'Error Message: ' + error_message();
+        print '=============================================================';
+    end catch
+end
